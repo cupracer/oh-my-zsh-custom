3454b25c16a8e3366b7e4940e15c83be520b5071
Thomas Schulte <thomas.schulte@suse.com>
2021-03-17 08:38:47 +0100

    add simple download script for required fonts

---
+++ tools/fetch-fonts.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+curl -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+curl -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+curl -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+curl -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+
